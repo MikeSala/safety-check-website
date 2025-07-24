a514149a892d95df099baabaef03e53d581e37db
Maciej Sala <maciek.a.sala@gmail.com>
2025-07-24 11:49:29 +0200

    About Us page: improved content structure & polish localization

---
+++ pages/about-us/content.pl.ts
@@ -0,0 +1,11 @@
+export const AboutUsPageContent = {
+  title: "O nas",
+  description:
+    "Jesteśmy firmą średniej wielkości specjalizującą się w przeglądach i konserwacji instalacji elektrycznych oraz gazowych na terenie całej Polski. Dzięki wieloletniemu doświadczeniu w sektorze mieszkaniowym i komercyjnym oferujemy kompleksowe usługi z zakresu bezpieczeństwa i zgodności z przepisami, dbając o bezpieczeństwo Twojej nieruchomości.",
+
+  seo: {
+    title: "O nas",
+    description:
+      "Działamy na terenie całej Polski, świadcząc kompleksowe usługi z zakresu bezpieczeństwa i zgodności z przepisami. Nasz zespół składa się z wykwalifikowanych i doświadczonych specjalistów.",
+  },
+};
--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -3,15 +3,16 @@ import { NextSeo } from "next-seo";
 import { ReactElement } from "react";
 import { Banner } from "~/components/Banner";
 import Button, { ButtonColor } from "~/components/Button";
+import { InspectionAreas } from "~/components/InspectionAreas";
 import { ResponsiveImage } from "~/components/ResponsiveImage";
 import { MainLayout } from "~/components/layouts/MainLayout";
 import { NextPageWithLayout } from "~/pages/_app";
-import { Icons8checkmark } from "~/src/components/icons";
+import { AboutUsPageContent as content } from "./content.pl";
 
 const AboutPage: NextPageWithLayout = () => {
   return (
     <>
-      <Banner title="About Us" />
+      <Banner title={content.seo.title} />
       <div className="flex flex-grow flex-wrap lg:flex-nowrap">
         <ResponsiveImage
           className="order-1 lg:order-1 lg:h-auto lg:w-1/2"
@@ -20,50 +21,23 @@ const AboutPage: NextPageWithLayout = () => {
           sizes="(min-width: 1024px) 50vw, 100vw"
         />
         <div className="order-1 flex flex-col justify-center gap-4 px-4 py-8 sm:px-6 sm:py-12 lg:w-1/2 lg:px-16 lg:py-20">
-          <h2 className="font-bold h2">
-            Residential & Commercial Safety Checks in Melbourne
-          </h2>
-          <p>
-            RCSC is a medium-sized safety compliance company based in Melbourne,
-            offering extensive electrical and gas maintenance and safety
-            services across all Victorian suburbs. With a wealth of experience
-            in both residential and commercial sectors, RCSC is qualified to
-            assist with a broad spectrum of safety and compliance issues,
-            ensuring the safety of your property.
-          </p>
+          <h2 className="font-bold h2">{content.title}</h2>
+          <p>{content.description}</p>
           <Button
-            href="/contact-us"
+            href="/kontakt"
             color={ButtonColor.Primary}
             className="mb-10 mt-10 w-fit"
             size="xl"
             outline
           >
-            Contact Us <ArrowLongRightIcon className="ml-2 h-7 w-7" />{" "}
+            Skontaktuj się z nami
+            <ArrowLongRightIcon className="ml-2 h-7 w-7" />{" "}
           </Button>
-          Our team is ready to conduct inspections that meet all legislative
-          standards in these Melbourne areas:{" "}
-          <ul className="mb-2 mt-2 list-disc">
-            <li className="mb-2 flex h-8 items-center">
-              <Icons8checkmark className="mr-2 h-7 w-7 flex-shrink-0" />
-              Melbourne City/Inner suburbs
-            </li>
-            <li className="mb-2  flex h-8 items-center">
-              <Icons8checkmark className="mr-2 h-7 w-7 flex-shrink-0" />
-              Mornington Peninsula
-            </li>
-            <li className="mb-2  flex h-8 items-center">
-              <Icons8checkmark className="mr-2 h-7 w-7 flex-shrink-0" />
-              South East
-            </li>
-            <li className="mb-2  flex h-8 items-center">
-              <Icons8checkmark className="mr-2 h-7 w-7 flex-shrink-0" />
-              Bayside
-            </li>
-            <li className=" flex h-8 items-center">
-              <Icons8checkmark className="mr-2 h-7 w-7 flex-shrink-0" />
-              Westernport
-            </li>
-          </ul>{" "}
+
+          <InspectionAreas
+            ctaHref="zarezerwuj-przeglad"
+            ctaLabel="Zarezerwuj przegląd"
+          />
         </div>
       </div>
     </>
@@ -74,8 +48,8 @@ AboutPage.getLayout = function GetLayout(page: ReactElement) {
   return (
     <MainLayout>
       <NextSeo
-        title="About Us"
-        description="RCSC operates Victoria-wide, providing comprehensive safety and compliance services with its highly-qualified, experienced team."
+        title={content.seo.title}
+        description={content.seo.description}
       />
       {page}
     </MainLayout>
