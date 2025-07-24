70153add7a2c7b0a08f1a02b5dede3e48bc9a99f
Maciej Sala <maciek.a.sala@gmail.com>
2025-07-24 12:35:12 +0200

    Book now page: localization changes

--- pages/book-now/index.tsx
+++ pages/book-now/index.tsx
@@ -1,36 +1,20 @@
 import { NextSeo } from "next-seo";
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import { Banner } from "~/components/Banner";
-import PropertyComplianceForm from "~/components/PropertyComplianceForm";
+import { ContactForm } from "~/components/ContactForm";
 import { ServiceBoxes } from "~/components/ServiceBoxes";
 import { MainLayout } from "~/components/layouts/MainLayout";
-import { NextPageWithLayout } from "~/pages/_app";
-import Link from "next/link";
 import { MarginLayout } from "~/components/layouts/MarginLayout";
+import { NextPageWithLayout } from "~/pages/_app";
 
 const BookPage: NextPageWithLayout = () => {
   return (
     <div>
-      <Banner title="Book Now" />
+      <Banner title="Zarezerwuj Przegląd" />
 
-      {/* Form */}
-      {/*<PropertyComplianceForm />*/}
-
-      {/* Temporary message until connected with new Cindirect in #72 */}
       <MarginLayout className="max-w-prose flex-col gap-2">
-        <p className={"mb-4"}>
-          Due to system upgrades, online bookings are currently unavailable.
-        </p>
-        <p>
-          Instead, please contact us directly at{" "}
-          <Link
-            href={`tel:${process.env.NEXT_PUBLIC_TEL_LINK}`}
-            className="link"
-          >
-            {process.env.NEXT_PUBLIC_TEL_LINK}
-          </Link>{" "}
-        </p>
+        <ContactForm />
       </MarginLayout>
 
       <ServiceBoxes />
@@ -42,8 +26,8 @@ BookPage.getLayout = function GetLayout(page: ReactElement) {
   return (
     <MainLayout>
       <NextSeo
-        title="Book Now | RCSC - Residential & Commercial Safety Checks"
-        description="Our team is ready to help you keep your business or property safe. Book now for fast, reliable service."
+        title="Zarezerwuj Przegląd"
+        description="Nasz zespół jest gotowy pomóc Ci zadbać o bezpieczeństwo Twojej firmy lub mienia. Zarezerwuj już teraz, aby skorzystać z niezawodnej usługi."
       />
       {page}
     </MainLayout>
