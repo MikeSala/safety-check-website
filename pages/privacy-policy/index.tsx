7f6debf31a96dd61ed499c2dbf5b42059a5e3b37
Maciej Sala <maciek.a.sala@gmail.com>
2025-07-24 10:46:50 +0200

    Privacy page: polish localization & minor changes

--- pages/privacy-policy/index.tsx
+++ pages/privacy-policy/index.tsx
@@ -9,192 +9,129 @@ import { NextPageWithLayout } from "~/pages/_app";
 const PrivacyPolicy: NextPageWithLayout = () => {
   return (
     <>
-      <Banner title="Privacy Policy" />
+      <Banner title="Polityka Prywatności" />
 
       <MarginLayout className="flex-col sm:mt-20 sm:py-1">
         <div className="p-4">
-          <h3 className="mb-4 text-center font-bold h1">Privacy Policy</h3>
-        </div>
-        <p className="mb-10">
-          RCSC is committed to providing quality services to you and this policy
-          outlines our ongoing obligations to you in respect of how we manage
-          your Personal Information.
-        </p>
-        <h3 className="mb-10 mt-20 text-center font-bold h2">
-          What is Personal Information and why do we collect it?
-        </h3>
-        <p className="mb-2">
-          Personal Information is data or an opinion that identifies an
-          individual. Examples of Personal Information we collect includes
-          names, addresses, email addresses and phone numbers.
-        </p>
-        <p className="mb-2">
-          This Personal Information is obtained in many ways from you and third
-          parties (including real estate agents, landlords and tenants) by, for
-          example, telephone, email, via our website at
-          https://www.przegladinstalacji.pl, from your website, from cookies and
-          other electronic tools and/or customer surveys.
-        </p>
-        <p className="mb-2">
-          We do not guarantee website links or the policy of authorised third
-          parties.
-        </p>
-        <p className="mb-2">
-          We collect your Personal Information for the primary purpose of
-          providing our services to you, providing information to our clients
-          and marketing. We may also use your Personal Information for secondary
-          purposes closely related to the primary purpose, in circumstances
-          where you would reasonably expect such use or disclosure. You may
-          unsubscribe from our mailing/marketing lists at any time by contacting
-          us in writing.
-        </p>
-        <p className="mb-2">
-          When we collect Personal Information we will, where appropriate and
-          where possible, explain to you why we are collecting the information
-          and how we plan to use it.
-        </p>
-        <div className="p-4">
-          <h3 className="mb-10 mt-20 text-center font-bold h2">
-            Other information
+          <h3 className="mb-4 text-center font-bold h1">
+            {privacyPolicyContent.sections[0].title}
           </h3>
         </div>
-        <p className="mb-2">
-          We may gather information from you through our electronic systems such
-          as your browser type, operating system, IP address, and/or the
-          referring domain name from which you access this site (e.g.,
-          www.google.com).
-        </p>
-        <p className="mb-2">
-          We may collect information about your browsing behaviour, such as the
-          number of visits, the date you visited, and the duration of your use
-          of our digital services. You may opt out by adjusting your internet
-          browser settings. However, this may limit the functionality and
-          personalisation of some parts of our platform.
-        </p>
-        <p className="mb-2">
-          {" "}
-          We may electronically monitor the success of any marketing campaign
-          from which you have not opted out.{" "}
-        </p>
-        <p className="mb-10">
-          We may also receive anonymised data about performance and errors while
-          you use our digital platform.
-        </p>
+        <p className="mb-10">{privacyPolicyContent.sections[0].description}</p>
+
         <h3 className="mb-10 mt-20 text-center font-bold h2">
-          Sensitive Information
+          {privacyPolicyContent.sections[1].title}
         </h3>
         <p className="mb-2">
-          {" "}
-          Sensitive information is defined in the Privacy Act to include
-          information or opinion about such things as an individual&#39;s racial
-          or ethnic origin, political opinions, membership of a political
-          association, religious or philosophical beliefs, membership of a trade
-          union or other professional body, criminal record or health
-          information.{" "}
+          {privacyPolicyContent.sections[1].description[0]}
         </p>
         <p className="mb-2">
-          We do not ordinarily expect to hold Sensitive information. However,
-          should we fall into possession of Sensitive information, such
-          information will be used by us only:
-        </p>{" "}
-        <ul className="mb-2 list-disc pl-5">
-          <li>For the primary purpose for which it was obtained.</li>
-          <li>
-            For a secondary purpose that is directly related to the primary
-            purpose.
-          </li>
-          <li>With your consent; or where required or authorised by law.</li>
-        </ul>
+          {privacyPolicyContent.sections[1].description[1]}
+        </p>
+        <p className="mb-2">
+          {privacyPolicyContent.sections[1].description[2]}
+        </p>
+        <p className="mb-2">
+          {privacyPolicyContent.sections[1].description[3]}
+        </p>
+
         <h3 className="mb-10 mt-20 text-center font-bold h2">
-          Disclosure of Personal Information
+          {privacyPolicyContent.sections[2].title}
         </h3>
         <p className="mb-2">
-          Your Personal Information may be disclosed in a number of
-          circumstances including the following:
+          {privacyPolicyContent.sections[2].description[0]}
         </p>
-        <ul className="mb-2 list-disc pl-5">
-          <li>Third parties where you consent to the use or disclosure; and</li>
-          <li>Where required or authorised by law.</li>
-        </ul>
+        <p className="mb-2">
+          {privacyPolicyContent.sections[2].description[1]}
+        </p>
+        <p className="mb-2">
+          {privacyPolicyContent.sections[2].description[2]}
+        </p>
+
         <h3 className="mb-10 mt-20 text-center font-bold h2">
-          Security of Personal Information
+          {privacyPolicyContent.sections[3].title}
         </h3>
         <p className="mb-2">
-          Your Personal Information is stored in a manner that reasonably
-          protects it from misuse and loss and from unauthorized access,
-          modification or disclosure.
+          {privacyPolicyContent.sections[3].description[0]}
         </p>
-        <p className="mb-2">
-          When your Personal Information is no longer needed for the purpose for
-          which it was obtained, we will take reasonable steps to destroy or
-          permanently de-identify your Personal Information. However, most of
-          the Personal Information is or will be stored in client files which
-          will be kept by us for a minimum of 7 years.
-        </p>
-        <p>
-          To the extent it is practical, you may use synonymies when dealing
-          with us.
-        </p>
+        {privacyPolicyContent.sections[3].list && (
+          <ul className="mb-2 list-disc pl-5">
+            {privacyPolicyContent.sections[3].list.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        )}
         <h3 className="mb-10 mt-20 text-center font-bold h2">
-          Access to your Personal Information
+          {privacyPolicyContent.sections[4].title}
         </h3>
         <p className="mb-2">
-          You may access the Personal Information we hold about you and update
-          or correct it, subject to certain exceptions. If you wish to access
-          your Personal Information, please contact us in writing.
+          {privacyPolicyContent.sections[4].description[0]}
         </p>
-        <p>
-          RCSC may charge an administrative fee for providing a copy of your
-          Personal Information. Page 3 In order to protect your Personal
-          Information, we may require identification from you before releasing
-          the requested information.
-        </p>
+
+        {privacyPolicyContent.sections[4].list && (
+          <ul className="mb-2 list-disc pl-5">
+            {privacyPolicyContent.sections[4].list.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        )}
+
         <h3 className="mb-10 mt-20 text-center font-bold h2">
-          Maintaining the Quality of your Personal Information
+          {privacyPolicyContent.sections[5].title}
         </h3>
         <p className="mb-2">
-          It is important to us that your Personal Information is up to date. We
-          will take reasonable steps to make sure that your Personal Information
-          is accurate, complete and up-to-date. However, if you find that the
-          information we have is not up to date or is inaccurate, please advise
-          us as soon as practicable so we can update our records and ensure we
-          can continue to provide quality services to you.
+          {privacyPolicyContent.sections[5].description[0]}
         </p>
+        <p>{privacyPolicyContent.sections[5].description[1]}</p>
+
         <h3 className="mb-10 mt-20 text-center font-bold h2">
-          Transferring Information Overseas
+          {privacyPolicyContent.sections[6].title}
         </h3>
         <p className="mb-2">
-          We will only share personal information about an individual with a
-          foreign entity if we believe, in good faith, that the receiving party
-          is subject to laws (including contracts with us) that effectively
-          uphold data protection principles equivalent to this policy.
+          {privacyPolicyContent.sections[6].description[0]}
         </p>
-        <h3 className="mb-10 mt-20 text-center font-bold h2">Policy Updates</h3>
-        <p className="mb-2">
-          This Policy was last updated on 22 January 2024. This Policy may
-          change from time to time and is available on our website.
-        </p>
+
         <h3 className="mb-10 mt-20 text-center font-bold h2">
-          Privacy Policy Complaints and Enquiries
+          {privacyPolicyContent.sections[7].title}
         </h3>
         <p className="mb-2">
-          If you have any queries or complaints about our Privacy Policy, please
-          contact us at:
-        </p>{" "}
-        <p className="mb-2"> 82 Bardia Street, Seaford, Victoria </p>
+          {privacyPolicyContent.sections[7].description[0]}
+        </p>
+
+        <h3 className="mb-10 mt-20 text-center font-bold h2">
+          {privacyPolicyContent.sections[8].title}
+        </h3>
+        <p className="mb-2">
+          {privacyPolicyContent.sections[8].description[0]}
+        </p>
+
+        <h3 className="mb-10 mt-20 text-center font-bold h2">
+          {privacyPolicyContent.sections[9].title}
+        </h3>
+        <p className="mb-2">
+          {privacyPolicyContent.sections[9].description[0]}
+        </p>
+
+        <h3 className="mb-10 mt-20 text-center font-bold h2">
+          {privacyPolicyContent.sections[10].title}
+        </h3>
+        <p className="mb-2">
+          {privacyPolicyContent.sections[10].description[0]}
+        </p>
+        <p className="mb-2">{privacyPolicyContent.address}</p>
         <Link
           className="cursor-pointer text-blue-500 hover:text-blue-400"
-          href={`tel:${process.env.NEXT_PUBLIC_TEL_LINK}`}
+          href={`tel:${privacyPolicyContent.phone}`}
         >
-          {process.env.NEXT_PUBLIC_TEL_LINK}
+          {privacyPolicyContent.phone}
         </Link>
         <Link
           className="mb-20 cursor-pointer text-blue-500 hover:text-blue-400"
-          href={`mailto:${process.env.NEXT_PUBLIC_EMAIL_LINK}`}
+          href={`mailto:${privacyPolicyContent.email}`}
           target="_blank"
           rel="noreferrer"
         >
-          {process.env.NEXT_PUBLIC_EMAIL_LINK}
+          {privacyPolicyContent.email}
         </Link>
       </MarginLayout>
     </>
@@ -214,3 +151,91 @@ PrivacyPolicy.getLayout = function GetLayout(page: ReactElement) {
 };
 
 export default PrivacyPolicy;
+
+export const privacyPolicyContent = {
+  updated: "22 stycznia 2024",
+  address: "ul. Przykładowa 1, 00-000 Warszawa",
+  phone: process.env.NEXT_PUBLIC_TEL_LINK,
+  email: process.env.NEXT_PUBLIC_EMAIL_LINK,
+  sections: [
+    {
+      title: "Polityka Prywatności",
+      description:
+        "Firma Przegląd Instalacji zobowiązuje się do świadczenia usług na najwyższym poziomie. Niniejsza polityka prywatności wyjaśnia, w jaki sposób gromadzimy, wykorzystujemy i chronimy Twoje dane osobowe zgodnie z przepisami RODO oraz ustawą o ochronie danych osobowych.",
+    },
+    {
+      title: "Jakie dane osobowe zbieramy i dlaczego?",
+      description: [
+        "Dane osobowe to wszelkie informacje umożliwiające identyfikację osoby fizycznej. Mogą to być: imię i nazwisko, adres e-mail, numer telefonu, adres zamieszkania itp.",
+        "Dane te pozyskujemy za pośrednictwem formularzy na stronie internetowej www.przegladinstalacji.pl, e-maila, telefonu oraz ciasteczek i innych narzędzi analitycznych. Dane mogą być także przekazane przez strony trzecie (np. zarządców nieruchomości).",
+        "Główne cele przetwarzania danych to: świadczenie usług, kontakt z klientami oraz działania marketingowe. W każdej chwili możesz zrezygnować z otrzymywania informacji marketingowych, kontaktując się z nami.",
+        "W miarę możliwości poinformujemy Cię, dlaczego potrzebujemy danych i w jaki sposób będą one wykorzystywane.",
+      ],
+    },
+    {
+      title: "Inne informacje",
+      description: [
+        "Możemy gromadzić informacje o przeglądarce, systemie operacyjnym, adresie IP oraz źródle wejścia na naszą stronę. Możemy analizować sposób korzystania z serwisu (częstotliwość odwiedzin, czas spędzony na stronie itp.). Część funkcji strony może nie działać prawidłowo po wyłączeniu obsługi ciasteczek.",
+        "Możemy również analizować skuteczność działań marketingowych, jeśli użytkownik nie zrezygnował z udziału w kampanii.",
+        "Możemy zbierać zanonimizowane dane o błędach i wydajności działania serwisu.",
+      ],
+    },
+    {
+      title: "Dane wrażliwe",
+      description: [
+        "Dane wrażliwe to m.in. informacje o pochodzeniu rasowym lub etnicznym, poglądach politycznych, stanie zdrowia, przynależności do związków zawodowych itd.",
+        "Nie planujemy przetwarzać takich danych, ale jeśli się to zdarzy, dane te będą wykorzystywane wyłącznie:",
+      ],
+      list: [
+        "w celu, dla którego zostały przekazane,",
+        "w sposób bezpośrednio powiązany z celem podstawowym,",
+        "za Twoją zgodą lub jeśli wymaga tego prawo.",
+      ],
+    },
+    {
+      title: "Udostępnianie danych osobowych",
+      description: ["Dane osobowe mogą zostać udostępnione:"],
+      list: [
+        "stronom trzecim za zgodą użytkownika,",
+        "w przypadkach wymaganych przez prawo.",
+      ],
+    },
+    {
+      title: "Bezpieczeństwo danych osobowych",
+      description: [
+        "Dane przechowujemy w sposób zabezpieczający przed nieuprawnionym dostępem, modyfikacją lub utratą. Po zakończeniu celu przetwarzania dane są usuwane lub anonimizowane, z wyjątkiem danych, które muszą być przechowywane przez co najmniej 5 lat zgodnie z przepisami.",
+        "W miarę możliwości możesz posługiwać się pseudonimem.",
+      ],
+    },
+    {
+      title: "Dostęp do danych osobowych",
+      description: [
+        "Masz prawo wglądu do swoich danych, ich aktualizacji i poprawiania. W tym celu skontaktuj się z nami pisemnie. Przed udzieleniem dostępu możemy poprosić o potwierdzenie tożsamości.",
+      ],
+    },
+    {
+      title: "Aktualność danych",
+      description: [
+        "Dbamy o to, by Twoje dane były aktualne. Jeśli zauważysz, że dane są nieaktualne lub błędne, poinformuj nas o tym, byśmy mogli dokonać stosownych poprawek.",
+      ],
+    },
+    {
+      title: "Przekazywanie danych poza Europejski Obszar Gospodarczy",
+      description: [
+        "Dane osobowe mogą być przekazywane poza EOG jedynie wówczas, gdy odbiorca zapewnia odpowiedni poziom ochrony danych zgodny z RODO (np. przez odpowiednie klauzule umowne).",
+      ],
+    },
+    {
+      title: "Aktualizacje polityki",
+      description: [
+        "Ostatnia aktualizacja polityki: 22 stycznia 2024 r. Zastrzegamy sobie prawo do wprowadzania zmian. Zaktualizowana wersja będzie dostępna na naszej stronie internetowej.",
+      ],
+    },
+    {
+      title: "Kontakt i skargi",
+      description: [
+        "W sprawach dotyczących danych osobowych skontaktuj się z nami:",
+      ],
+    },
+  ],
+};
