45c14e97f81ef44952293b4c2956b76a76f2a516
Maciej Sala <maciek.a.sala@gmail.com>
2025-07-24 10:38:32 +0200

    Legislation page: improved content structure & polish localization

---
+++ pages/legislation/content.pl.ts
@@ -0,0 +1,18 @@
+export const legislationPageContent = {
+  title: `Przeglądy okresowe – obowiązki prawne właścicieli i zarządców`,
+  description: `Obowiązujące w Polsce przepisy nakładają na właścicieli i zarządców nieruchomości obowiązek regularnych przeglądów instalacji w celu zapewnienia bezpieczeństwa użytkowników lokali.`,
+  subTitle: `Do najważniejszych obowiązków należą:`,
+  paragraphs: [
+    `Przegląd instalacji elektrycznej co najmniej raz na 5 lat. Obejmuje on sprawdzenie stanu technicznego, zabezpieczeń oraz środków ochrony przeciwporażeniowej.`,
+    ` Coroczny przegląd instalacji gazowej – kontrola szczelności oraz stanu technicznego przewodów i urządzeń gazowych.`,
+    `Obowiązek zapewnienia sprawnych czujników dymu w lokalach mieszkalnych oraz ich testowanie co najmniej raz w roku.`,
+  ],
+  note: `Więcej szczegółów znajduje się poniżej.`,
+  source: `Źródła:`,
+  linkAnkor1: `Ustawa – Prawo budowlane (Dz.U. 1994 Nr 89 poz. 414)`,
+  linkAnkor2: `Rozporządzenie Ministra Infrastruktury z dnia 12 kwietnia 2002 r.`,
+  seo: {
+    title: `Wymagania prawne`,
+    description: `Dowiedź się więcej na temat wymogów prawnych związanych z przeglądami okresowymi właścicieli i zarządców nieruchomości`,
+  },
+};
--- pages/legislation/index.tsx
+++ pages/legislation/index.tsx
@@ -2,74 +2,64 @@ import { NextSeo } from "next-seo";
 import Link from "next/link";
 import { ReactElement } from "react";
 import { Banner } from "~/components/Banner";
-import FaqComponent from "~/components/FAQ/FaqComponent";
-import FaqSectionsData from "~/components/FAQ/FaqData";
+import FaqComponent from "~/components/FAQ/Faq";
+import FaqSectionsData from "~/components/FAQ/faqContent";
 import { MainLayout } from "~/components/layouts/MainLayout";
 import { MarginLayout } from "~/components/layouts/MarginLayout";
 import { NextPageWithLayout } from "~/pages/_app";
 import { Icons8checkmark } from "~/src/components/icons";
+import { legislationPageContent as content } from "./content.pl";
 
 const selectedId = [151, 152, 153];
 
 const FaqPage: NextPageWithLayout = () => {
   return (
     <>
-      <Banner title="Legislation" />
+      <Banner title={content.seo.title} />
 
       <MarginLayout className="flex-col sm:mt-20 sm:py-1">
         <div className="p-4">
-          <h3 className="mb-4 text-center font-bold h2">
-            The Residential Tenancies Amendment Act and 2021 Regulations
-          </h3>
-          <p className="mb-2">
-            Regulations identify and mandate new safety-related activities for
-            Victoria&apos;s residential properties.
-          </p>
+          <h3 className="mb-4 text-center font-bold h2">{content.title}</h3>
+          <p className="mb-2">{content.description}</p>
 
-          <h3 className="mb-2 mt-4 text-lg">
-            All rental providers must ensure the following:
-          </h3>
-          <ul className="mb-2 list-disc">
-            <li className="h-18 mb-2 mr-2 flex items-center">
-              <Icons8checkmark className="mr-4 h-7 w-7 flex-shrink-0" />
-              All smoke detectors must be installed correctly, be in the correct
-              locations, be operational, and undergo annual testing according to
-              the manufacturer&apos;s guidelines.
-            </li>
-            <li className="h-18 mb-2 mr-2 flex items-center">
-              <Icons8checkmark className="mr-4 h-7 w-7 flex-shrink-0" />
-              Electricity and gas safety inspections must be performed every two
-              years. However, if an electrical or gas safety check has not been
-              completed within the last two years at the time the renter
-              occupies the premises, the rental provider must schedule an
-              electrical and gas safety inspection as soon as is practicable.
-            </li>
+          <h3 className="mb-2 mt-4 text-lg">{content.subTitle}</h3>
+          <ul className="mb-2 mt-6 list-disc">
+            {content.paragraphs.map((p, i) => (
+              <li key={i} className="h-18 mb-2 mr-2 flex items-center">
+                <Icons8checkmark className="mr-4 h-7 w-7 flex-shrink-0" />
+                {p}
+              </li>
+            ))}
           </ul>
 
-          <p className="font-semibold">
-            More details related to this topic can be found down below.
-          </p>
+          <p className="mt-6 font-semibold">{content.note}</p>
         </div>
         <div className="px-4">
           <FaqComponent sections={FaqSectionsData} selectedIds={selectedId} />
         </div>
-        <strong>Sources:</strong>
-        <Link
-          href="https://www.legislation.vic.gov.au/in-force/statutory-rules/residential-tenancies-regulations-2021/001"
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-          className="cursor-pointer text-blue-500 hover:text-blue-400"
-        >
-          https://www.legislation.vic.gov.au/in-force/statutory-rules/residential-tenancies-regulations-2021/001
-        </Link>
-        <Link
-          href="https://www.vba.vic.gov.au/consumers/residential-tenancies-regulations-2021"
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-          className="mb-10 cursor-pointer text-blue-500 hover:text-blue-400"
-        >
-          https://www.vba.vic.gov.au/consumers/residential-tenancies-regulations-2021
-        </Link>
+        <div className="mb-10">
+          <p>
+            <strong>{content.source}</strong>
+          </p>
+          <p>
+            <Link
+              href="https://isap.sejm.gov.pl/isap.nsf/download.xsp/WDU19940830429/U/D19940429Lj.pdf"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              className="cursor-pointer text-blue-500 hover:text-blue-400"
+            >
+              {content.linkAnkor1}
+            </Link>
+          </p>
+          <Link
+            href="https://isap.sejm.gov.pl/isap.nsf/DocDetails.xsp?id=WDU20020750690"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            className="cursor-pointer text-blue-500 hover:text-blue-400"
+          >
+            {content.linkAnkor2}
+          </Link>
+        </div>
       </MarginLayout>
     </>
   );
@@ -79,8 +69,8 @@ FaqPage.getLayout = function GetLayout(page: ReactElement) {
   return (
     <MainLayout>
       <NextSeo
-        title="Legislation - Safety Inspection and Compliance"
-        description="Stay informed with our comprehensive legislation information page. Our services comply with all laws and regulations."
+        title={content.seo.title}
+        description={content.seo.description}
       />
       {page}
     </MainLayout>
