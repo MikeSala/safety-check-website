cece5ba476b24e06769ce0b82b1ba32ca32f63e7
Maciej Sala <maciek.a.sala@gmail.com>
2025-07-30 19:37:37 +0200

    Form component: optional titleId & company adresess change

--- components/FormComponent.tsx
+++ components/FormComponent.tsx
@@ -22,23 +22,25 @@ const titles: Record<TitleId, string> = {
 };
 
 interface FormComponentProps {
-  titleId: TitleId;
+  titleId?: TitleId;
 }
 
 const FormComponent: React.FC<FormComponentProps> = ({ titleId }) => {
-  const title = titles[titleId];
+  const title = titleId ? titles[titleId] : "";
 
   return (
     <>
-      <article className="flex h-[100px] flex-col items-center justify-center gap-4 bg-gray-200 px-4 py-12 text-center text-2xl h3 sm:py-24 lg:px-8">
-        <h2 className="font-semibold h3">{title}</h2>
-      </article>
-      <MarginLayout className="mt-10 items-center justify-center">
+      {titleId && (
+        <article className="flex h-[100px] flex-col items-center justify-center gap-4 bg-gray-200 px-4 py-12 text-center text-2xl h3 sm:py-24 lg:px-8">
+          <h2 className="font-semibold h3">{title}</h2>
+        </article>
+      )}
+      <MarginLayout className="mt-10 mb-10 items-center justify-center">
         <div className="gap-12 md:grid md:grid-cols-2">
           <iframe
             className="h-full w-full"
             title="maps"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3138.9527097571467!2d145.13328191579782!3d-38.11803457969842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad674ab7d057495%3A0xc8c874fe20a0df6f!2s82%20Bardia%20Ave%2C%20Seaford%20VIC%203198%2C%20Australia!5e0!3m2!1sen!2sus!4v1673845456634!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2564.942865752377!2d19.898584876970316!3d50.06027477151416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165b095a9606a3%3A0x87206338a9a2951c!2sLudwika%20Rydla%2C%2030-001%20Krak%C3%B3w!5e0!3m2!1spl!2spl!4v1722325256744!5m2!1spl!2spl"
             allowFullScreen
             loading="eager"
             referrerPolicy="no-referrer-when-downgrade"
