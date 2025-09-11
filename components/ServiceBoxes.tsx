5028ab4eb3b1b15d99096f1531d9a16f33d74b51
Maciej Sala <maciek.a.sala@gmail.com>
2025-09-11 12:43:04 +0200

    ServiceBoxes: localization changes

--- components/ServiceBoxes.tsx
+++ components/ServiceBoxes.tsx
@@ -9,13 +9,20 @@ import { ViewportContext } from "~/providers/ViewportProvider";
 const recommendedServices: SERVICE_KEYS[] = [SERVICE_KEYS.FullServicePackage];
 const displayedServices = SERVICES.filter((service) => service.description);
 
+const serviceBoxesContent = {
+  title: `Nasze usługi przeglądów bezpieczeństwa`,
+  price: `Oszczędź 140 zł`,
+  note1: `* Zwróć uwagę: 10% zniżki dla właścicieli posiadających wiele nieruchomości zarejestrowanych na jedno nazwisko lub jedną firmę.`,
+  note2: `** Jednorazowa opłata serwisowa w wysokości 250 zł netto za każdą zgłoszoną nieruchomość na wynajem.`,
+};
+
 export const ServiceBoxes: React.FC = () => {
   const { isMobile } = useContext(ViewportContext);
 
   return (
-    <article className="mt-20 rounded border bg-gray-200 px-8 sm:px-16 lg:px-20">
+    <article className="mt-20 mb-20 rounded border bg-gray-200 px-8 sm:px-16 lg:px-20">
       <h3 className="mb-20 mt-20 px-2 text-center font-semibold h2 sm:px-4 lg:px-0">
-        Our Safety Check Services
+        {serviceBoxesContent.title}
       </h3>
       <div className="flex flex-col gap-2 px-2 pb-6">
         <div
@@ -29,12 +36,12 @@ export const ServiceBoxes: React.FC = () => {
                 <Link href={service.address} key={service.label}>
                   <article
                     className={clsx(
-                      "group relative flex cursor-pointer flex-col justify-between overflow-hidden rounded-lg bg-white px-2 py-4 transition-transform duration-300 hover:scale-105 hover:border-primary-600 hover:shadow-md",
+                      "group relative flex cursor-pointer flex-col justify-between overflow-hidden rounded-lg border-2 bg-white px-2 py-4 transition-transform duration-300 hover:scale-105 hover:border-sky-700 hover:shadow-md",
                       isMobile
                         ? "h-52 w-80"
                         : "md:h-64 md:w-72 2xl:h-72 2xl:w-80",
                       recommendedServices.includes(service.value)
-                        ? "border border-red-600 shadow-sm"
+                        ? "border-2 border-red-600 shadow-sm"
                         : "shadow-sm"
                     )}
                   >
@@ -50,14 +57,14 @@ export const ServiceBoxes: React.FC = () => {
                         isMobile ? "text-md" : "text-2xl"
                       } flex items-center justify-center font-bold`}
                     >
-                      ${service.price / 100}
-                      <span className="text-md ml-1 font-medium">/ex gst</span>
+                      {service.price / 100}
+                      <span className="text-md ml-1 font-medium">PLN</span>
                     </h4>
 
                     {service.label === "Full Service Package" && (
                       <>
-                        <div className="absolute right-4 top-4 w-full origin-top translate-x-1/2 rotate-45 gap-1 bg-red-600 py-2 text-center text-sm font-semibold text-white transition duration-300 group-hover:bg-primary-600">
-                          Save $140
+                        <div className="absolute right-4 top-4 w-full origin-top translate-x-1/2 rotate-45 gap-1 border-2 border-sky-800 bg-red-600 py-2 text-center text-sm font-semibold text-white transition duration-300 group-hover:bg-sky-800">
+                          {serviceBoxesContent.price}
                         </div>
 
                         <div className="text-center text-sm">
@@ -74,14 +81,14 @@ export const ServiceBoxes: React.FC = () => {
                     )}
 
                     <Button
-                      className="font-base ml-8 mr-8 inline-flex items-center rounded bg-white px-6 group-hover:bg-primary-600 group-hover:text-white sm:mt-4"
+                      className="font-base ml-8 mr-8 inline-flex items-center rounded bg-white px-6 group-hover:bg-sky-800 group-hover:text-white sm:mt-4"
                       color={ButtonColor.Primary}
                       size={isMobile ? "sm" : "lg"}
-                      title={`Learn more about ${service.label}`}
+                      title={`Dowiedz się więcej - ${service.label}`}
                       outline
                       type="button"
                     >
-                      Learn more{" "}
+                      Więcej{" "}
                       <ArrowLongRightIcon
                         className={`${isMobile ? "h-6 w-6" : "h-8 w-8"}`}
                       />
@@ -93,12 +100,10 @@ export const ServiceBoxes: React.FC = () => {
         </div>
         <div className="mb-4 mt-4 flex flex-col items-center justify-center gap-4 rounded border p-4 sm:px-14">
           <small className="text-md text-center font-semibold h5">
-            * Please note a 10% discount applies for landlords with multiple
-            properties listed in one name or one company name.
+            {serviceBoxesContent.note1}
           </small>
           <small className="text-md text-center font-semibold h5">
-            ** A one-time only maintenance fee of $250 ex GST per rental
-            property you subscribe with.
+            {serviceBoxesContent.note2}
           </small>
         </div>
       </div>
